515dd5c7d4696602ad4de2d42042c2d89b95b951
Hamza <hamza.bahamdan@gmail.com>
2023-10-16 16:56:48 +0300

    Player Movement and Joystick
    - added new input system
    - implement player movement through WASD and joystick in screen

---
+++ Random Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private Controls _controls;
+    
+    private float movementX;
+    private Vector2 movement;
+
+    private void Awake()
+    {
+        _controls = new Controls();
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        _controls.Enable();
+        _controls.Player.Move.performed += OnMovementPerformed;
+        _controls.Player.Move.canceled += OnMovementCanceled;
+    }
+
+    void Update()
+    {
+        GetPlayerInput();
+        MovePlayer();
+    }
+
+    private void OnDisable()
+    {
+        _controls.Disable();
+        _controls.Player.Move.performed -= OnMovementPerformed;
+        _controls.Player.Move.canceled -= OnMovementCanceled;
+    }
+    
+    private void OnMovementPerformed(InputAction.CallbackContext obj)
+    {
+        Debug.Log(obj.ReadValue<Vector2>());
+        movementX = obj.ReadValue<Vector2>().x;
+    }
+
+    private void OnMovementCanceled(InputAction.CallbackContext obj)
+    {
+        movementX = 0;
+    }
+
+    private void GetPlayerInput()
+    {
+        // movementX = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void MovePlayer()
+    {
+        _rb.velocity = new Vector2(movementX * moveSpeed, _rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        _rb.AddForce(new Vector2(0,jumpForce),ForceMode2D.Impulse);
+
+    }
+}
