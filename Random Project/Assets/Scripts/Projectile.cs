c95e5c27fa6349df7e982c6c5856d4da897f3df2
Hamza <hamza.bahamdan@gmail.com>
2023-10-26 11:39:57 +0300

    projectile interactions and lifetime death

--- Random Project/Assets/Scripts/Projectile.cs
+++ Random Project/Assets/Scripts/Projectile.cs
@@ -1,9 +1,13 @@
+using System.Collections;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
     public Vector2 direction;
     public float speed;
+
+    [SerializeField] private float lifetime;
+    [SerializeField] private float damage;
 
     private Rigidbody2D _rigidbody2D;
 
@@ -14,6 +18,7 @@ public class Projectile : MonoBehaviour
 
     void Start()
     {
+        // Move the projectile in the desired direction
         _rigidbody2D.AddForce(direction * speed, ForceMode2D.Impulse);
         
         // Calculate the angle in radians
@@ -24,5 +29,28 @@ public class Projectile : MonoBehaviour
         
         // rotate the projectile to be facing the direction
         transform.rotation = Quaternion.Euler(0,0,angleInDegrees-90);
+        
+        // destroy the projectile after a certain period of time
+        Die(lifetime);
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<Enemy_Base>().GetHit(damage);
+            Die(0);
+        }
+    }
+
+    public void Die(float duration)
+    {
+        StartCoroutine(DieCoroutine(duration));
+    }
+
+    private IEnumerator DieCoroutine(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        Destroy(gameObject);
+    } 
 }
