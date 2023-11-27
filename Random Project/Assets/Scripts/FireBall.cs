aca2f42df672e929d949c2e75dd47a8e9bb8793f
sal1122 <84595877+sal1122@users.noreply.github.com>
2023-11-27 11:20:34 +0300

    Death animation

--- Random Project/Assets/Scripts/FireBall.cs
+++ Random Project/Assets/Scripts/FireBall.cs
@@ -50,6 +50,7 @@ public class FireBall : MonoBehaviour
     {
         if (col.CompareTag("Enemy"))
         {
+            FindAnyObjectByType<AudioManager>().Play("FireDestroy");
             col.GetComponent<Enemy_Base>().GetHit(damage);
             Destroy(gameObject);
         }
