bc68605fbe7e43220f62dd6ac8fe857090f0b855
sal1122 <84595877+sal1122@users.noreply.github.com>
2023-12-07 15:13:57 +0300

    New organizing

---
+++ Random Project/Assets/Scripts/Enemy_Base.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Enemy_Base : Flashable
+{
+    //public Collectible.XpLevel _xpLevel = Collectible.XpLevel.One;
+    
+    //Refs
+    Transform playerPos;
+    [SerializeField] GameObject deathPar;
+
+    //Attributes
+    [SerializeField] private int health;
+    [SerializeField] float speed;
+    [SerializeField] private int damage;
+    
+    [SerializeField] private float speedLevelUpRate;
+    [SerializeField] private float healthLevelUpRate;
+    bool stopDmg;
+
+    public override void Awake()
+    {
+        base.Awake();
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void OnEnable()
+    {
+        //XpManager.OnPlayerLevelUp += LevelUpSpeed;
+    }
+
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, playerPos.position) > 0.2f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnDisable()
+    {
+        //XpManager.OnPlayerLevelUp -= LevelUpSpeed;
+    }
+
+    public void GetHit(int dmg)
+    {
+        //print("Reduce Health");
+        health -= dmg;
+
+        if (health <= 0)
+        {
+            //GameObject.FindObjectOfType<Enemy_Spawner>().UpdateEnemyNumber(false);
+            //ScrapSpawner.NewEnemyDied(_xpLevel, transform.position);
+            Instantiate(deathPar, transform.position, Quaternion.identity);
+            FindObjectOfType<CinemaShake>().Shake(2f, 0.2f);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Flash();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (stopDmg)
+                return;
+
+            print("Get Hit");
+            StartCoroutine(CanDmg());
+            
+            // deal damage to the player
+            col.GetComponent<PlayerHealth>().TakeDamage(damage);
+        }
+    }
+
+    private IEnumerator CanDmg()
+    {
+        stopDmg = true;
+        yield return new WaitForSeconds(0.5f);
+        stopDmg = false;
+    }
+
+    private void LevelUpSpeed()
+    {
+        speed += speedLevelUpRate;
+    }
+
+    // private void LevelUpHealth()
+    // {
+    //     health += healthLevelUpRate;
+    // }
+}
---
+++ Random Project/Assets/Scripts/ProjectileBase.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class ProjectileBase : MonoBehaviour
+{
+    Rigidbody2D rb => GetComponent<Rigidbody2D>();
+
+    [SerializeField] public float speed, currentSpeed;
+    [HideInInspector] public bool aim;
+
+    private void Awake()
+    {
+        Destroy(gameObject, 5);
+    }
+
+    void FixedUpdate()
+    {
+        if (aim)
+        {
+            Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+            Vector3 difference = player.position - transform.position;
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, rotZ + -90);
+            aim = false;
+        }
+
+        if (currentSpeed < speed)
+        {
+            currentSpeed += 0.3f;
+        }
+
+        rb.velocity = transform.up * currentSpeed;
+    }
+
+    // private void OnTriggerEnter2D(Collider2D col)
+    // {
+    //     if (col.gameObject.layer == 6)
+    //     {
+    //         Destroy(gameObject);
+    //     }
+    // }
+}
